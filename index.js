1f22ae3fccc59f97d63a5ea6d22ecd0ceb1d699a
Oliver Steele <steele@osteele.com>
2018-04-12 14:47:03 -0400

    Add lint config and fix warnings

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import './src/Main.scss';
 import Elm from './src/Main.elm';
-const app = Elm.Main.embed(document.getElementById('app'), {
-    apiServer: process.env.API_SERVER || 'http://localhost:3000'
+
+Elm.Main.embed(document.getElementById('app'), {
+  apiServer: process.env.API_SERVER || 'http://localhost:3000',
 });
