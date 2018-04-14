e768688e725fa0f6bdc82399c2a7317038c1d7e2
Oliver Steele <steele@osteele.com>
2018-04-14 14:52:58 -0400

    Update the title

--- poi.config.js
+++ poi.config.js
@@ -1,16 +1,16 @@
 module.exports = {
-    presets: [
-        require('poi-preset-elm')({
-            loaderOptions: {
-                debug: false,
-                forceWatch: true
-            }
-        })
-    ],
-    env: {
-        API_SERVER: process.env.API_SERVER
-    },
-    html: {
-        title: 'ABE Event'
-    }
+  presets: [
+    require('poi-preset-elm')({
+      loaderOptions: {
+        debug: false,
+        forceWatch: true,
+      },
+    }),
+  ],
+  env: {
+    API_SERVER: process.env.API_SERVER,
+  },
+  html: {
+    title: 'ABE Schedule',
+  },
 };
